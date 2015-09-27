18003cb4e0c6fb752bb9ff6221a784a3e3709c8c
Jared Harkins <jared.harkins@students.makeschool.com>
2015-09-27 02:56:42 -0700

    removing flask restful, basic flask hello world server

--- ruse_api.py
+++ ruse_api.py
@@ -1,39 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
-# from game import Game
-
 app = Flask(__name__)
-api = Api(app)
 
 
-class CreateGame(Resource):
-    def get(self):
-        return {'gameid': 1}
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-
-class JoinGame(Resource):
-    def get(self):
-        return {'gameid': 1}
-
-
-class Turn(Resource):
-    def get(self):
-        return {'turn': 1}
-
-    def post(self):
-        return {'turn': 1}
-
-
-class GameInfo(Resource):
-    def get(self):
-        return {'gameid': 1,
-                'players': ['none']}
-
-
-api.add_resource(CreateGame, '/create')
-api.add_resource(JoinGame, '/join')
-api.add_resource(Turn, '/game/turn')
-api.add_resource(GameInfo, '/game')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
