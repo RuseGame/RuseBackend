a65985317b107e02325e86c025b9bec64861adb6
Jared Harkins <jared.harkins@students.makeschool.com>
2015-09-27 02:31:37 -0700

    rough api outline

--- ruse_api.py
+++ ruse_api.py
@@ -1,15 +1,39 @@
 from flask import Flask
 from flask_restful import Resource, Api
+# from game import Game
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class CreateGame(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'gameid': 1}
 
-api.add_resource(HelloWorld, '/')
+
+class JoinGame(Resource):
+    def get(self):
+        return {'gameid': 1}
+
+
+class Turn(Resource):
+    def get(self):
+        return {'turn': 1}
+
+    def post(self):
+        return {'turn': 1}
+
+
+class GameInfo(Resource):
+    def get(self):
+        return {'gameid': 1,
+                'players': ['none']}
+
+
+api.add_resource(CreateGame, '/create')
+api.add_resource(JoinGame, '/join')
+api.add_resource(Turn, '/game/turn')
+api.add_resource(GameInfo, '/game')
 
 if __name__ == '__main__':
     app.run(debug=True)
