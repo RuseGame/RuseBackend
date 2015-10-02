381a4075662e90e904ab5534364a7c169b9632bb
Jared Harkins <jared.harkins@students.makeschool.com>
2015-10-02 16:53:13 -0700

    flushing out initial api

--- ruse_api.py
+++ ruse_api.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from game import Game
 app = Flask(__name__)
+
+games = {}
+game_id_counter = 0
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return jsonify({"hello": "world"})
 
+
+@app.route('/games')
+def get_games():
+    global games
+    return jsonify({"current games": str(games.keys())})
+
+
+@app.route('/games/create')
+def make_game():
+    global game_id_counter
+    game_id_counter += 1
+    games[str(game_id_counter)] = Game(game_id_counter)
+    return jsonify({"game_id": str(game_id_counter)})
 
 if __name__ == '__main__':
     app.run()
