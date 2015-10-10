1917f49d932f032fc69626d4392f5d2efb77ce8a
Jared Harkins <jared.harkins@students.makeschool.com>
2015-10-09 18:27:48 -0700

    initial experiments with cookies and server side events

--- ruse_api.py
+++ ruse_api.py
@@ -1,14 +1,52 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response, request, make_response
 from game import Game
+import time
 app = Flask(__name__)
 
 games = {}
+game_eventers = {}
 game_id_counter = 0
+player_uid = 0
+
+
+class ServerSideEventer:
+    def __init__(self):
+        self.maps = {}
+        self.needs_update = {}
+
+    def register(self, cookie):
+        self.maps[cookie] = 0
+        self.needs_update[cookie] = True
+
+    def update(self, cookie, dictionary):
+        pass
+
+    def get_update(self, cookie):
+        self.needs_update[cookie] = False
+        return "success"
+
+
+def event_stream(eventer, cookie):
+    old = time.time()
+    while True:
+        new = time.time()
+        if eventer.needs_update[cookie]:
+            message = eventer.get_update(cookie)
+            yield 'data: %s\n\n' % message
+        if new - old > 5:
+            old = new
+            yield 'data: none\n\n'
+        time.sleep(0.5)
 
 
 @app.route('/')
 def hello_world():
-    return jsonify({"hello": "world"})
+    global player_uid
+    resp = make_response(jsonify({"hello": "world"}))
+    if not request.cookies.get('player_id'):
+        resp.set_cookie('player_id', str(player_uid))
+        player_uid += 1
+    return resp
 
 
 @app.route('/games')
@@ -19,10 +57,22 @@ def get_games():
 
 @app.route('/games/create')
 def make_game():
-    global game_id_counter
+    global game_id_counter, games, game_eventers
     game_id_counter += 1
-    games[str(game_id_counter)] = Game(game_id_counter)
+    eventer = ServerSideEventer()
+    eventer.register(request.cookies.get("player_id"))
+    games[str(game_id_counter)] = Game(game_id_counter, eventer)
+    game_eventers[str(game_id_counter)] = eventer
     return jsonify({"game_id": str(game_id_counter)})
 
+
+@app.route('/<game_id>/stream')
+def stream(game_id):
+    global game_eventers
+    player_cookie = request.cookies.get("player_id")
+    return Response(event_stream(game_eventers[game_id], player_cookie),
+                    mimetype="text/event-stream")
+
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(threaded=True)
