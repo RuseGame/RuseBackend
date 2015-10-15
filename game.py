b6902e465086a0f79229fd384e871df70dbe87bd
Nick Munoz-McDonald <nicolas.munoz.mcdonald@students.makeschool.com>
2015-10-14 18:15:23 -0700

    Complete game code (very ugly, must be refactored/redone)

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 from random import shuffle
 
 NUM_PLAYERS = 5
-ALIASES = ["Pink", "Green", "Blue", "White", "Orange"]
+ALIASES = ("Pink", "Green", "Blue", "White", "Orange")
+MOVE_TYPES = ("send", "spoof", "wiretap", "ambush")
 
 
 class Game:
@@ -39,7 +40,7 @@ class Game:
         Give everyone an alias and target, then start the turn
         """
 
-        aliases = ALIASES[:]
+        aliases = list(ALIASES)
         shuffle(aliases)
         player_list = list(self.players.values())
         shuffle(player_list)
@@ -54,17 +55,102 @@ class Game:
             player.inbox[0].append("Hello " + str(player) + ",\n" +
                                    "Your target is Mr. " + player.target + ".")
             self.emitter.update(player.cookie, player._to_dict())
+        self.start_turn()
 
     def start_turn(self):
-        # TODO: send turn start message
-        pass
+        self.turns.append(Turn(len(self.turns)+1))
+        for player in self.players.values():
+            player.inbox.append([])
+
+    def _validate_moves(self, mover_alias, move_list):
+        action_points = 4
+        other_players = [a for a in ALIASES if a != mover_alias]
+        for move in move_list:
+            move_type = move.get("move_type")
+            if move_type in MOVE_TYPES:
+                if move_type == "send":
+                    send_from = move.get("from")
+                    send_to = move.get("to")
+                    if send_from != mover_alias or send_to not in other_players:
+                        return False
+                    action_points -= 1
+                elif move_type == "spoof":
+                    spoofed_from = move.get("from")
+                    spoofed_to = move.get("to")
+                    spoofer = move.get("spoofer")
+                    if spoofer != mover_alias or spoofed_to == spoofed_from:
+                        return False
+                    if spoofed_to not in ALIASES or spoofed_from not in ALIASES:
+                        return False
+                    action_points -= 2
+                elif move_type == "wiretap":
+                    target = move.get("target")
+                    tapper = move.get("tapper")
+                    direction = move.get("direction")
+                    if tapper != mover_alias or target not in ALIASES:
+                        return False
+                    if direction != "incoming" and direction != "outgoing":
+                        return False
+                    action_points -= 3
+                elif move_type == "ambush":
+                    target = move.get("target")
+                    attacker = move.get("attacker")
+                    if attacker != mover_alias or target not in other_players:
+                        return False
+                    action_points -= 4
+            else:
+                return False
+        if action_points < 0:
+            return False
+        return True
 
     def process_moves(self, player_cookie, move_list):
-        pass
+        if self.turns == []:
+            return False
+        current_turn = self.turns[-1]
+        submitting_player = self.players[player_cookie]
+        if self._validate_moves(submitting_player.alias, move_list):
+            if current_turn.submit(submitting_player.alias, move_list):
+                if current_turn.missing_players() == set():
+                    self.resolve_turn()
+                return True
+        return False
 
     def resolve_turn(self):
-        pass
+        ending_turn = self.turns[-1]
+        alias_map = {player.alias: player
+                     for player
+                     in self.players.values()}
+        for message in ending_turn.messages:
+            report = "\"" + str(message.get("message")) + "\"\n- Mr. " + str(message.get("from"))
+            alias_map[message.get("to")].inbox[-1].append(report)
+        for wiretap in ending_turn.wiretaps:
+            report = "From your wiretap:\n"
+            in_out = "to" if wiretap.get("direction") == "incoming" else "from"
+            for message in ending_turn.messages:
+                if message.get(in_out) == wiretap.get("target"):
+                    report += "Mr. " + str(message.get("to")) + ":\n" + str(message.get("message")) + "\n- Mr. " + str(message.get("from"))
+            alias_map[wiretap.get("tapper")].inbox[-1].append(report)
+        hits = {alias: []
+                for alias
+                in alias_map.keys()}
+        for attack in ending_turn.attacks:
+            hits[attack.get("target")].append(attack.get("attacker"))
+        for target, attackers in hits.items():
+            if len(attackers) > 1:
+                alias_map[target].inbox[-1].append("You have been killed.")
+                self.end_game()
+                return
+            elif len(attackers) > 0:
+                alias_map[target].inbox[-1].append("You were unsucessfully attacked by Mr. " + attackers[0])
+        for cookie, player in self.players.items():
+            self.emitter.update(cookie, player._to_dict())
+        self.start_turn()
 
+    def end_game(self):
+        for cookie, player in self.players.items():
+            player.inbox.append(["Game Over."])
+            self.emitter.update(cookie, player._to_dict())
 
 class Player:
     def __init__(self, cookie, nickname):
@@ -89,12 +175,25 @@ class Player:
 class Turn:
     def __init__(self, turn_number):
         self.turn_number = turn_number
-        self.submitted = [False] * NUM_PLAYERS
+        self.players = set(ALIASES)
+        self.submitted = set()
         self.messages = []
         self.wiretaps = []
         self.attacks = []
 
+    def submit(self, alias, moves):
+        if alias not in self.submitted:
+            for move in moves:
+                move_type = move.get("move_type")
+                if move_type == "send" or move_type == "spoof":
+                    self.messages.append(move)
+                elif move_type == "wiretap":
+                    self.wiretaps.append(move)
+                elif move_type == "ambush":
+                    self.attacks.append(move)
+            self.submitted.add(alias)
+            return True
+        return False
 
-class Move:
-    def __init__(self, data):
-        self.data = data
+    def missing_players(self):
+        return self.players - self.submitted
--- test.py
+++ test.py
@@ -10,15 +10,45 @@ class MockEmitter:
 def run_tests():
     emitter = MockEmitter()
     players = ["Rick", "Morty", "Troy", "Abed", "Yolo", "Solo"]
-    test_cases = [("5 Players", players[:5]),
-                  ("Too Few (4) Players", players[:4]),
-                  ("Too Many (6) Players", players)]
+    test_cases = [("5 Players", players[:5])]
 
     for descrip, case in test_cases:
         print(descrip)
         ruse = game.Game(1, emitter)
         for cookie, name in enumerate(case):
             ruse.add_player(cookie, name)
+        for cookie, player in ruse.players.items():
+            move_list = [{"move_type": "send",
+                                         "to": player.target,
+                                         "from": player.alias,
+                                         "message": "Hi " + str(cookie)}]
+            if cookie % 2 == 1:
+                move_list.append({"move_type": "spoof",
+                                  "to": player.alias,
+                                  "from": player.target,
+                                  "spoofer": player.alias,
+                                  "message": "spoofed by " + str(player.alias)})
+            else:
+                move_list.append({"move_type": "wiretap",
+                                  "tapper": player.alias,
+                                  "target": player.target,
+                                  "direction": "outgoing"})
+            ruse.process_moves(cookie, move_list)
+
+        for cookie, player in ruse.players.items():
+            move_list = []
+            if cookie % 2 == 1:
+                move_list.append({"move_type": "ambush",
+                                  "attacker": player.alias,
+                                  "target": "White"})
+            else:
+                move_list.append({"move_type": "spoof",
+                                  "spoofer": player.alias,
+                                  "to": player.alias,
+                                  "from": player.target,
+                                  "message": "spoofed by " + str(player.alias)})
+            ruse.process_moves(cookie, move_list)
+
         print("\n")
 
 
